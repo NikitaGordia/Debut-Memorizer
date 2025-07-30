60fe9079e55f009578f5392a583e7692f5a30ec8
Mykyta Hordia <nikita.gordia@gmail.com>
2025-07-30 19:48:18 +0000

    Add Explorer with cache, precalculate games with depth=7

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func, desc
 from sqlalchemy.orm import sessionmaker
 from .models import Game, Base
 
@@ -24,3 +24,29 @@ def add_games(games: list[Game]):
     with Session() as session:
         session.add_all(games)
         session.commit()
+
+
+def get_next_move_distribution(opening_uci: str) -> dict[str, int]:
+    with Session() as session:
+        opening_uci = opening_uci.strip()
+
+        num_opening_moves = len(opening_uci.split()) if opening_uci else 0
+        next_move_index = num_opening_moves + 1
+
+        next_move_expr = func.split_part(Game.uci, " ", next_move_index).label(
+            "next_move"
+        )
+
+        query = session.query(next_move_expr, func.count(Game.id).label("move_count"))
+
+        if opening_uci:
+            query = query.filter(Game.uci.like(f"{opening_uci} %"))
+
+        query = query.group_by(next_move_expr)
+
+        query = query.filter(next_move_expr != "")
+
+        query = query.order_by(desc("move_count"))
+
+        results = query.all()
+        return {move: count for move, count in results}
---
+++ src/explorer.py
@@ -0,0 +1,99 @@
+import argparse
+from . import db
+import diskcache as dc
+import tqdm
+from concurrent.futures import ThreadPoolExecutor
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class Explorer:
+    def __init__(
+        self, cache_path: str, num_workers: int = 2, stop_threshold: float = 0.05
+    ):
+        self.stop_threshold = stop_threshold
+        self.executor = ThreadPoolExecutor(
+            max_workers=num_workers, thread_name_prefix="ExprorerWorker"
+        )
+        self.cache = dc.Cache(cache_path)
+        self.futures = {}
+
+    def _explore(self, uci):
+        if uci in self.cache:
+            return self.cache[uci]
+        moves = db.get_next_move_distribution(uci)
+        self.cache[uci] = moves
+        return moves
+
+    def submit_job(self, uci: str) -> None:
+        future = self.executor.submit(self._explore, uci)
+        self.futures[uci] = future
+
+    def get_result(self, uci: str) -> dict[str, int]:
+        future = self.futures.pop(uci, None)
+        if not future:
+            raise KeyError(f"UCI '{uci}' not found or already retrieved.")
+        return future.result()
+
+    def submit_and_get(self, uci: str) -> dict[str, int]:
+        self.submit_job(uci)
+        return self.get_result(uci)
+
+    def _recursive_explore(self, uci: str, depth: int, max_depth: int, pbar: tqdm.tqdm):
+        dst = self.get_result(uci)
+        pbar.update(1)
+        total = sum(dst.values())
+        if total == 0:
+            return
+
+        if depth + 1 > max_depth:
+            return
+
+        for move, count in dst.items():
+            if count / total < self.stop_threshold:
+                continue
+            new_uci = f"{uci} {move}" if uci else move
+            self.submit_job(new_uci)
+            self._recursive_explore(new_uci, depth + 1, max_depth, pbar)
+
+    def explore(self, uci: str, depth: int) -> dict[str, int]:
+        self.submit_job(uci)
+
+        pbar = tqdm.tqdm(desc="🧭 Exploring", unit="positions")
+        self._recursive_explore(uci, 0, depth, pbar)
+        pbar.close()
+
+    def shutdown(self):
+        print("Shutting down the thread pool. Waiting for active jobs to finish...")
+        self.executor.shutdown(wait=True)
+        print("All workers have been shut down.")
+
+
+def explore(depth: int, num_workers: int) -> dict[str, int]:
+    explorer = Explorer(os.environ.get("EXPLORER_CACHE_PATH"), num_workers)
+    explorer.explore("", depth)
+    explorer.shutdown()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Multi-threaded explorer of moves distribution.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--num-workers",
+        type=int,
+        default=os.cpu_count() // 2,
+        help="Number of worker processes to use for parsing.",
+    )
+    parser.add_argument(
+        "--depth",
+        type=int,
+        default=3,
+        help="Depth of the exploration.",
+    )
+    args = parser.parse_args()
+
+    explore(args.depth, args.num_workers)
